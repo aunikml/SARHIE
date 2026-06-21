8225f5ebacfc145dfee6a16e4f9c1ad100368330
aunikml <hamimmalahsan@gmail.com>
2026-06-21 13:26:32 +0600

    Remove root-level duplicate backend files and update .gitignore

--- api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import TeamMember, Partner, Initiative, Event
-
-admin.site.register(TeamMember)
-admin.site.register(Partner)
-admin.site.register(Initiative)
-admin.site.register(Event)
